524e1a6a571a012c5a79032f24464bbbb62cedb3
Russel Savage <savage.russel@gmail.com>
2022-03-18 14:27:08 -0500

    chore: add example scss module to nextjs web app

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,11 @@
 import { Button } from 'ui';
 import { exampleService } from 'utils';
+import { example } from './index.module.scss';
 
 export default function Web() {
 	console.log(exampleService());
 	return (
-		<div>
+		<div className={example}>
 			<h1>Web</h1>
 			<Button />
 		</div>
---
+++ apps/web/types.d.ts
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '*.svg' {
+	export const ReactComponent: any;
+	export const content: any;
+}
+
+declare module '*.module.css' {
+	const classes: { [key: string]: string };
+	export default classes;
+}
+
+declare module '*.module.scss' {
+	const classes: { [key: string]: string };
+	export default classes;
+}
+
+export {};
