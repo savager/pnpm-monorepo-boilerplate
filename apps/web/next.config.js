31b6be45328a177f0445bb27aeb04bfd81902bcf
Russel Savage <savage.russel@gmail.com>
2022-03-18 12:56:42 -0500

    chore: update eslint config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,6 @@
-const withTM = require("next-transpile-modules")(["ui"]);
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withTM = require('next-transpile-modules')(['ui']);
 
 module.exports = withTM({
-  reactStrictMode: true,
+	reactStrictMode: true,
 });
